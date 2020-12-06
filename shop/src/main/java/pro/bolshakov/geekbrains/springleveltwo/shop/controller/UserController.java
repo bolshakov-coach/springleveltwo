00e076d6a19c8b92b2942e76c475d2bc0c2616a3
Aleksandr <mail.bolshakov@gmail.com>
2020-12-06 20:40:44 +0300

    add example of error handler

--- shop/src/main/java/pro/bolshakov/geekbrains/springleveltwo/shop/controller/UserController.java
+++ shop/src/main/java/pro/bolshakov/geekbrains/springleveltwo/shop/controller/UserController.java
@@ -21,6 +21,9 @@ public class UserController {
 
     @GetMapping
     public String userList(Model model){
+        if(1 == 1){
+            throw new RuntimeException("test of error handling");
+        }
         model.addAttribute("users", userService.getAll());
         return "userList";
     }
