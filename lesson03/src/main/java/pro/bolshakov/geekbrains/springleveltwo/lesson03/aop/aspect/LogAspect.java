634cdc9440306406ef7336d9ac380901a1b3d501
Aleksandr <mail.bolshakov@gmail.com>
2020-12-08 18:52:20 +0300

    example to measure methods

--- lesson03/src/main/java/pro/bolshakov/geekbrains/springleveltwo/lesson03/aop/aspect/LogAspect.java
+++ lesson03/src/main/java/pro/bolshakov/geekbrains/springleveltwo/lesson03/aop/aspect/LogAspect.java
@@ -5,8 +5,8 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
-@Aspect
-@Component
+//@Aspect
+//@Component
 public class LogAspect {
 
     @Pointcut("execution(* pro.bolshakov.geekbrains.springleveltwo.lesson03.aop.service..*.*(..))")
--- lesson03/src/main/java/pro/bolshakov/geekbrains/springleveltwo/lesson03/aop/service/RndService.java
+++ lesson03/src/main/java/pro/bolshakov/geekbrains/springleveltwo/lesson03/aop/service/RndService.java
@@ -1,13 +1,18 @@
 package pro.bolshakov.geekbrains.springleveltwo.lesson03.aop.service;
 
 import org.springframework.stereotype.Service;
+import pro.bolshakov.geekbrains.springleveltwo.lesson03.aop.aspect.MeasureMethod;
 
 import java.util.UUID;
 
 @Service
 public class RndService {
+    @MeasureMethod
     public String getString(){
         System.out.println("called RndService.getString");
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {}
         return UUID.randomUUID().toString();
     }
 }
