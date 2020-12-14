9c364e3282d6bc5887d69bc2e0860fbe6b435979
Aleksandr <mail.bolshakov@gmail.com>
2020-12-14 19:04:07 +0300

    lesson05 example with two clients

--- lesson05/client1/src/main/java/pro/bolshakov/geekbrains/spring2/lesson05/client1/AppCloudClientNews.java
+++ lesson05/client1/src/main/java/pro/bolshakov/geekbrains/spring2/lesson05/client1/AppCloudClientNews.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppClient1 {
+public class AppCloudClientNews {
     public static void main(String[] args) {
-        SpringApplication.run(AppClient1.class, args);
+        SpringApplication.run(AppCloudClientNews.class, args);
     }
 }
---
+++ lesson05/clientusers/src/main/java/pro/bolshakov/geekbrains/spring2/lesson05/client1/AppCloudClientUsers.java
@@ -0,0 +1,11 @@
+package pro.bolshakov.geekbrains.spring2.lesson05.client1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCloudClientUsers {
+    public static void main(String[] args) {
+        SpringApplication.run(AppCloudClientUsers.class, args);
+    }
+}
