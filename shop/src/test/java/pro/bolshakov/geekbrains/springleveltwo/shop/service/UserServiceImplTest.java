dbc7851b86938776073c363523a72a253b8b3fed
Aleksandr <mail.bolshakov@gmail.com>
2020-12-20 19:57:06 +0300

    lesson07 example with exception and verify of invoke methods

--- shop/src/test/java/pro/bolshakov/geekbrains/springleveltwo/shop/service/UserServiceImplTest.java
+++ shop/src/test/java/pro/bolshakov/geekbrains/springleveltwo/shop/service/UserServiceImplTest.java
@@ -1,10 +1,12 @@
 package pro.bolshakov.geekbrains.springleveltwo.shop.service;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.Mockito;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import pro.bolshakov.geekbrains.springleveltwo.shop.dao.UserRepository;
 import pro.bolshakov.geekbrains.springleveltwo.shop.domain.User;
+import pro.bolshakov.geekbrains.springleveltwo.shop.dto.UserDto;
 
 import java.util.UUID;
 
@@ -75,6 +77,46 @@ class UserServiceImplTest {
         Assertions.assertEquals(expectedUser, actualUser);
 
         Assertions.assertNull(rndUser);
+
+    }
+
+    @Test
+    void checkSaveIncorrectPassword(){
+        //have
+        UserDto userDto = UserDto.builder()
+                .password("password")
+                .matchingPassword("another")
+                .build();
+
+        //execute
+        Assertions.assertThrows(RuntimeException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                userService.save(userDto);
+            }
+        });
+
+    }
+
+    @Test
+    void checkSave(){
+        //have
+        UserDto userDto = UserDto.builder()
+                .username("name")
+                .email("email")
+                .password("password")
+                .matchingPassword("password")
+                .build();
+
+        Mockito.when(passwordEncoder.encode(Mockito.anyString())).thenReturn("password");
+
+        //execute
+        boolean result = userService.save(userDto);
+
+        //check
+        Assertions.assertTrue(result);
+        Mockito.verify(passwordEncoder).encode(Mockito.anyString());
+        Mockito.verify(userRepository).save(Mockito.any());
 
     }
 }
