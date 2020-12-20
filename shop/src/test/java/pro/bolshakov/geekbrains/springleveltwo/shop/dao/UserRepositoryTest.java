7c397d3fd51b1112ea45a525f76805b6a07b3345
Aleksandr <mail.bolshakov@gmail.com>
2020-12-20 20:27:45 +0300

    lesson07 example of test with init sql scripts

--- shop/src/test/java/pro/bolshakov/geekbrains/springleveltwo/shop/dao/UserRepositoryTest.java
+++ shop/src/test/java/pro/bolshakov/geekbrains/springleveltwo/shop/dao/UserRepositoryTest.java
@@ -6,11 +6,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import pro.bolshakov.geekbrains.springleveltwo.shop.domain.Role;
 import pro.bolshakov.geekbrains.springleveltwo.shop.domain.User;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
+@SqlGroup({@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:initUsers.sql")})
 class UserRepositoryTest {
     @Autowired
     private TestEntityManager entityManager;
@@ -36,6 +40,21 @@ class UserRepositoryTest {
         Assertions.assertEquals(user.getName(), actualUser.getName());
         Assertions.assertEquals(user.getPassword(), actualUser.getPassword());
         Assertions.assertEquals(user.getEmail(), actualUser.getEmail());
+
+    }
+
+    @Test
+    void checkFindByNameAfterSql() {
+        //execute
+        User actualUser = userRepository.findFirstByName("admin");
+
+        //check
+        Assertions.assertNotNull(actualUser);
+        Assertions.assertEquals(5, actualUser.getId());
+        Assertions.assertEquals("admin", actualUser.getName());
+        Assertions.assertEquals("adminpass", actualUser.getPassword());
+        Assertions.assertEquals("admin@mail.ru", actualUser.getEmail());
+        Assertions.assertEquals(Role.ADMIN, actualUser.getRole());
 
     }
 }
